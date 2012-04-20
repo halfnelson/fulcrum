91165a79ffae5c634bdbdb24cd9e257e945f5f96
david <david@apollosoftware.com.au>
2012-04-20 10:57:56 +1000

    removed confirm! from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,6 @@
 
 user = User.create! :name => 'Test User', :initials => 'TU',
                     :email => 'test@example.com', :password => 'testpass'
-user.confirm!
 
 project = Project.create! :name => 'Test Project', :users => [user], :start_date => Time.now
 
